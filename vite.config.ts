a421bb0d05f47041db641f77b40903fa5cb218c0
IndraSaputraIdrus <indrasaputraidrus@gmail.com>
2026-09-13 01:52:48 +0800

    change adapter vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -17,7 +17,7 @@ export default defineConfig({
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter({
-				fallback: '404.html'
+				// fallback: '404.html'
 			})
 		})
 	]
